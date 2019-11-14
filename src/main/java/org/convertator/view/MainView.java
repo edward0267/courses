9aac10748b051daa88a018e89be55d56a6fa1c91
DzmitryZaitsau <dimlepus@gmail.com>
2019-11-14 22:15:04 +0300

    change main view

--- src/main/java/org/convertator/view/MainView.java
+++ src/main/java/org/convertator/view/MainView.java
@@ -3,6 +3,7 @@ package org.convertator.view;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -15,21 +16,25 @@ import java.awt.*;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        VerticalLayout todosList = new VerticalLayout(); // (1)
-        TextField taskField = new TextField(); // (2)
-        Button addButton = new Button("Add"); // (3)
-        addButton.addClickShortcut(Key.ENTER);
-        addButton.addClickListener(click -> {
-            // (4)
-            Checkbox checkbox = new Checkbox(taskField.getValue());
-            todosList.add(checkbox);
-        });
-        add( // (5)
-                new H1("Vaadin Todo"),
+        String from;
+        VerticalLayout todosList = new VerticalLayout();
+        ComboBox<String> select = new ComboBox<>("Select from unit");
+        select.setItems("Miles", "Meters", "Foots", "Inchs");
+        ComboBox<String> selectTo = new ComboBox<>("Select to unit");
+        selectTo.setItems("Miles", "Meters", "Foots", "Inchs");
+        TextField taskField = new TextField();
+        Button convertButton = new Button("Convert");
+        convertButton.addClickShortcut(Key.ENTER);
+
+
+        add(
+                new H1("Converter"),
                 todosList,
                 new HorizontalLayout(
+                        select,
+                        selectTo,
                         taskField,
-                        addButton
+                        convertButton
                 )
         );
     }
